3e570b5c183db671ac56205e27aaf7904dcd42ec
ik04 <ishaankhurana123@gmail.com>
2023-11-08 23:37:05 +0530

    fix: delete old client

--- client/web/src/app/context/GlobalContext.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-import { createContext } from "react";
-import { GlobalContextValue } from "./context";
-
-export const GlobalContext = createContext<Partial<GlobalContextValue>>({});
