f7e9bd3cde8fb9f8d69a0c55ed3a25f26791f284
ik04 <ishaankhurana123@gmail.com>
2023-12-30 17:01:52 +0530

    fix: fix landing responsiveness in phones

--- client/web/app/components/landing.tsx
+++ client/web/app/components/landing.tsx
@@ -4,19 +4,19 @@ export const Landing = () => {
   return (
     <div className="bg-primary bg-[url('/assets/KaizenKlassBg.png')] h-screen flex justify-center items-center">
       <div className="hero flex flex-col items-center">
-        <h1 className="font-display text-highlight text-[70px] md:text-[140px]">
+        <h1 className="font-display text-highlight text-center text-5xl md:text-[140px]">
           Kaizen Klass
         </h1>
-        <p className="text-highlightSecondary font-base text-center text-[20px] md:text-4xl">
+        <p className="text-highlightSecondary font-base text-center text-base md:text-4xl">
           Learn, Grow and Overcome Assignments
         </p>
-        <div className="buttons flex space-x-5 md:mt-10 mt-5">
-          <div className="cursor-default border border-highlight text-highlight md:w-[280px] text-xl w-[170px] md:text-3xl h-[60px] md:h-[75px] justify-center items-center flex rounded-full font-base">
+        <div className="buttons flex md:space-x-5 space-x-3 md:mt-10 mt-5">
+          <div className="cursor-default border border-highlight text-highlight md:w-[280px] text-base w-[120px] md:text-3xl h-[40px] md:h-[75px] justify-center items-center flex rounded-full font-base">
             Contribute
           </div>
           <Link
             to={"/home"}
-            className="cursor-default border border-highlight bg-highlight text-primary md:w-[280px] text-xl w-[170px] md:text-3xl h-[60px] md:h-[75px] justify-center items-center flex rounded-full font-base"
+            className="cursor-default border border-highlight bg-highlight text-primary md:w-[280px] text-base w-[120px] md:text-3xl h-[40px] md:h-[75px] justify-center items-center flex rounded-full font-base"
           >
             Check it out
           </Link>
