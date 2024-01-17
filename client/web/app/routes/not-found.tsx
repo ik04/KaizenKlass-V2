cac27386cb87a7a1b5b15083f948f284ebe5c114
ik04 <ishaankhurana123@gmail.com>
2024-01-17 11:54:26 +0530

    feat: normalise new links and finish not-found for mobile

--- client/web/app/routes/not-found.tsx
+++ client/web/app/routes/not-found.tsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "@remix-run/react";
 import React from "react";
+import { BackButton } from "~/components/backButton";
 import { Dashboard } from "~/components/dashboard";
 
 export default function notFound() {
@@ -8,7 +9,19 @@ export default function notFound() {
   return (
     <div className="bg-main h-screen">
       <Dashboard baseUrl={baseUrl}>
-        <div className=""></div>
+        <div className="h-full">
+          <div className="w-full p-3 flex bg-main justify-start items-center">
+            <BackButton />
+          </div>
+          <div className="flex justify-center items-center h-[70%] w-full">
+            <div className="flex flex-col  items-center justify-center">
+              <img src="/assets/error.png" className="" alt="" />
+              <div className="font-base text-highlightSecondary text-2xl font-bold">
+                Not Found
+              </div>
+            </div>
+          </div>
+        </div>
       </Dashboard>
     </div>
   );
