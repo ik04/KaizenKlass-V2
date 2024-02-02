cb8e1600155a7f6eed740ef0962be5af079cbb89
ik04 <ishaankhurana123@gmail.com>
2024-02-02 23:13:27 +0530

    feat: shift subjects from server to client side, add skeletons for loading state

--- client/web/app/routes/subjects._index.tsx
+++ client/web/app/routes/subjects._index.tsx
@@ -1,24 +1,47 @@
 import { useLoaderData } from "@remix-run/react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { Dashboard } from "~/components/dashboard";
 import { SplashScreen } from "~/components/splashScreen";
 import { SubjectCard } from "~/components/subjectCard";
+import { Skeleton } from "~/components/ui/skeleton";
 export default function subjects() {
-  //
-  const { subjects, baseUrl }: { subjects: Subject[]; baseUrl: string } =
-    useLoaderData();
+  const { baseUrl }: { baseUrl: string } = useLoaderData();
+  const [subjects, setSubjects] = useState<Subject[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  useEffect(() => {
+    const callSubjectsEnpoint = async () => {
+      const url = `${baseUrl}/api/v1/get-subjects`;
+      const resp = await axios.get(url);
+      setSubjects(resp.data.subjects);
+    };
+    callSubjectsEnpoint();
+  }, [baseUrl]);
   return (
     <div className="bg-main min-h-screen">
       <Dashboard baseUrl={baseUrl}>
         <div className="md:grid flex flex-col md:grid-cols-4">
-          {subjects.map((subject) => (
-            <div key={subject.subject} className="py-10">
-              <SubjectCard
-                subject={subject.subject}
-                uuid={subject.subject_uuid}
-              />
-            </div>
-          ))}
+          {!isLoading ? (
+            <>
+              {subjects.map((subject) => (
+                <div key={subject.subject} className="py-10">
+                  <SubjectCard
+                    subject={subject.subject}
+                    uuid={subject.subject_uuid}
+                  />
+                </div>
+              ))}
+            </>
+          ) : (
+            <>
+              {Array.from({ length: 12 }, (_, index) => (
+                <Skeleton
+                  key={index}
+                  className="p-5 mb-11 md:mb-14 h-28 md:p-2 border border-mainLighter md:w-80 md:h-80 rounded-3xl bg-mainLighter transition-all"
+                />
+              ))}
+            </>
+          )}
         </div>
       </Dashboard>
     </div>
@@ -27,10 +50,7 @@ export default function subjects() {
 // todo: make an actual dashboard with useful info to replace subjects page and make subjects page its own thing
 // todo: handle server errors (get help)
 export async function loader() {
-  const url = `${process.env.PUBLIC_DOMAIN}/api/v1/get-subjects`;
-  const resp = await axios.get(url);
   const data = {
-    subjects: resp.data.subjects,
     baseUrl: process.env.PUBLIC_DOMAIN,
   };
   console.log(process.env.PUBLIC_DOMAIN);
