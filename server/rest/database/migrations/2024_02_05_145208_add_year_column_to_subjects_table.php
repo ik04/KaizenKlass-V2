5357645bdfffc2b1e8e40811cb950e833e2a1236
ik04 <ishaankhurana123@gmail.com>
2024-02-05 15:54:47 +0530

    feat: update migrations, add years seeder and update subjects seeder

---
+++ server/rest/database/migrations/2024_02_05_145208_add_year_column_to_subjects_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('subjects', function (Blueprint $table) {
+            $table->unsignedBigInteger("year_id");
+            $table->foreign("year_id")->references("id")->on("years")->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('subjects', function (Blueprint $table) {
+            //
+        });
+    }
+};
